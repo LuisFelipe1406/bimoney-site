dd240fe1d8943da824963fd488e53e4555823837
LuisFelipe1406 <luisfelipestututi@gmail.com>
2024-09-04 17:48:56 -0300

    feat: change README and init top HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,9 @@ import MyHeader from "../components/MyHeader.vue";
 <template>
   <MyHeader />
   <div id="divMain">
+    <div id="divTopMain">
+      <!-- <img src="/img/main.jpg" alt="image-top"> -->
+    </div>
     <h1>Bem Vindo!</h1>
   </div>
 </template>
@@ -12,10 +15,21 @@ import MyHeader from "../components/MyHeader.vue";
 <style scoped>
 #divMain {
   position: relative;
-  height: 200vh;
-
-  top: 10vh;
 
   padding: 2vh;
+}
+
+#divTopMain {
+  width: 100vw;
+  height: 95vh;
+
+  position: absolute;
+  top: 0vh;
+  left: 0vh;
+
+  background-image: url("/img/main.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 </style>
