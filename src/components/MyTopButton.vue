b35f2d64a26527fccb71f3005d5205b4e3dc7e62
LuisFelipe1406 <luisfelipestututi@gmail.com>
2024-09-05 17:44:55 -0300

    feat: create MyTopButton; improve HomeView

---
+++ src/components/MyTopButton.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+import { ref } from 'vue';
+
+const props = defineProps({
+    buttonText: {
+        type: String,
+        required: true
+    },
+    buttonTextHover: {
+        type: String,
+        required: false
+    },
+    buttonIcon: {
+        type: String,
+        required: false
+    }
+});
+
+const hover = ref(false);                // Controla classes ao dar hover no botão
+const textButton = ref("Teste Agora");   // Texto do botão
+
+// Altera o texto ao dar hover no botão
+function hoverOn() {
+    textButton.value = "É Grátis!";
+    hover.value = true;
+}
+
+function hoverOff() {
+    textButton.value = "Teste Agora";
+    hover.value = false;
+}
+
+</script>
+
+<template>
+    <button @mouseenter="hoverOn" @mouseleave="hoverOff">
+        <span :class="{ 'hovered': hover }" id="btnText">{{ textButton }}</span>
+        <Icon icon="streamline:button-power-1-solid" :class="{ 'hovered': hover }" id="btnIcon" />
+    </button>
+</template>
+
+<style scope>
+button {
+    width: 30vh;
+
+    padding: 2vh;
+
+    color: var(--our-white);
+    background-color: var(--our-green);
+
+    font-size: 20px;
+    font-weight: 300;
+
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+
+    border: 0px;
+    border-radius: 5px;
+
+    transition: background-color 0.5s ease;
+}
+
+button:hover {
+    background-color: var(--our-dark-blue);
+}
+
+span {
+    color: var(--our-white);
+
+    cursor: pointer;
+
+    display: inline-block;
+    transition: transform 0.3s ease;
+}
+
+#btnText.hovered {
+    transform: translateX(30px);
+}
+
+#btnIcon {
+    margin-left: 2vh;
+
+    transition: transform 0.3s ease;
+}
+
+#btnIcon.hovered {
+    transform: translate(-100px);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,103 @@
 <script setup lang="ts">
 import MyHeader from "../components/MyHeader.vue";
+import MyTopButton from "../components/MyTopButton.vue";
 </script>
 
 <template>
   <MyHeader />
   <div id="divMain">
     <div id="divTopMain">
-      <!-- <img src="/img/main.jpg" alt="image-top"> -->
+      <div id="divTopShape"> </div>
+      <div id="divTopInfo">
+        <img src="/img/logo.png" alt="logo-top">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Sed egestas ante at nisl varius, quis lobortis orci faucibus.
+          Duis commodo viverra lectus et euismod. Pellentesque ac
+          hendrerit nisl. Nullam non elit nec.
+        </p>
+        <MyTopButton />
+      </div>
+      <div id="divTopVideo">
+        <h3>Confira nosso <span class="hDestaque">video de apresentação</span></h3>
+      </div>
     </div>
-    <h1>Bem Vindo!</h1>
+    <div id="divProduct">
+
+    </div>
   </div>
 </template>
 
 <style scoped>
-#divMain {
-  position: relative;
-
-  padding: 2vh;
-}
+#divMain {}
 
 #divTopMain {
   width: 100vw;
   height: 95vh;
 
-  position: absolute;
-  top: 0vh;
-  left: 0vh;
-
   background-image: url("/img/main.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  justify-content: center;
+  align-items: center;
+}
+
+#divTopShape {
+  width: 60%;
+  height: 95vh;
+
+  position: absolute;
+
+  background-color: var(--our-white);
+
+  clip-path: polygon(0 0, 100% 0, 70% 100%, 0% 100%);
+}
+
+#divTopInfo {
+  z-index: 1;
+
+  padding: 10% 25% 10% 25%;
+
+  display: grid;
+  grid-template-rows: 1fr;
+  row-gap: 3vh;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    max-width: 30vh;
+  }
+
+  p {
+    width: 28vh;
+
+    margin-bottom: 5vh;
+
+    text-align: left;
+    font-size: 20px;
+  }
+}
+
+#divTopVideo {
+  display: grid;
+  grid-template-rows: repeat(2, max-content);
+  row-gap: 3vh;
+
+  h3 {
+    color: var(--our-white);
+
+    font-weight: 300;
+  }
+}
+
+#divProduct {
+  width: 100vw;
+  height: 95vh;
+
+  background-color: var(--our-dark-blue)
 }
 </style>
